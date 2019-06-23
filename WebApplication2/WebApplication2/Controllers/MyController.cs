8b76258b26a1ee756e9a8bfe532eba3782352ac8
Виталий Паршаков <vaparshakov@edu.hse.ru>
2019-06-23 14:56:21 +0500

    WEbApp

---
+++ WebApplication2/WebApplication2/Controllers/MyController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class MyController : Controller
+    {
+        // GET: My
+        public ActionResult Index()
+        {
+            forMatrix formatrix = new forMatrix();
+            return View(formatrix);
+        }
+        [HttpPost] //вызывается после нажатия на кнопку
+        public ActionResult Index(forMatrix formatrix, int rows, int col)
+        {
+            formatrix.mas = new int[rows][];
+            for (int i = 0; i < rows; i++)
+            {
+                formatrix.mas[i] = new int[col];
+            }
+            formatrix.rows = rows;
+            formatrix.col = col;
+            return View(formatrix);
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class forMatrix
+    {
+        public int[][] mas { get; set; }
+        public int rows { get; set; }
+        public int col { get; set; }
+    }
+}
