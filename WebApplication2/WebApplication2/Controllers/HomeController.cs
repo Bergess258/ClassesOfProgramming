255aa32ac4af0f125949b51010b535c52657dc02
Виталий Паршаков <vaparshakov@edu.hse.ru>
2019-07-27 16:32:54 +0500

    MadeOnClass

--- WebApplication2/WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -9,8 +9,10 @@ namespace WebApplication2.Controllers
 {
     public class HomeController : Controller
     {
+        Base context = new Base();
         public ActionResult Index()
         {
+            context.Case.Co
             return View();
         }
 
@@ -30,8 +32,27 @@ namespace WebApplication2.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-
             return View();
+        }
+        public FileContentResult Image(string caseNAme)
+        {
+            List<Case> temp = context.Case.Where(p => p.Name == caseNAme).ToList();
+            if (temp != null && temp.Count != 0)
+            {
+                return new FileContentResult(temp[0].Image, "png");
+            }
+            throw new Exception("Such case not founded");
+
+        }
+        public FileContentResult WeaponImage(int Id)
+        {
+            List<Weapon> tempW = context.Weapon.Where(p => p.Id == 0).ToList();
+            //context.possSkinsInCase.Find(Id).Weapon.Image; possSkinsInCase поменять на название таблицы
+            if (tempW != null && tempW.Count != 0)
+            {
+                return new FileContentResult(tempW[0].Image, "png");
+            }
+            throw new Exception("Such case not founded");
         }
     }
 }
--- WebApplication2/WebApplication2/Startup.cs
+++ WebApplication2/WebApplication2/Startup.cs
@@ -1,14 +1,37 @@
 ﻿using Microsoft.Owin;
 using Owin;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using WebApplication2.Models;
 
 [assembly: OwinStartupAttribute(typeof(WebApplication2.Startup))]
 namespace WebApplication2
 {
     public partial class Startup
     {
+
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            //string fileName = @"C:\Users\parsh\source\repos\NewRepo\WebApplication2\WebApplication2\Content\img\512fx384f.png";
+            //FileInfo fileInfo = new FileInfo(fileName);
+            //long length = fileInfo.Length;
+            //FileStream reader = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            //BinaryReader br = new BinaryReader(reader);
+            //byte[] temp = br.ReadBytes((int)length);
+            //Base context = new Base();
+            //NameW nameW = context.NameW.Where(p => p.Id == 0).ToList()[0];
+            //SkinN Skin = context.SkinN.Where(p => p.Id == 0).ToList()[0];
+            //SkinN Skin = context.SkinN.Where(p => p.Id == 0).ToList()[0];
+            //Type type = context.Type.Where(p => p.Id == 0).ToList()[0];
+            //Weapon tempW = new Weapon();
+            //tempW.TypeId = 0;
+            //tempW.WeaponNId = 0;
+            //tempW.SkinNameId = 0;
+            //tempW.Image = temp;
+            //context.Weapon.Add(tempW);
+            //context.SaveChanges();
         }
     }
 }
